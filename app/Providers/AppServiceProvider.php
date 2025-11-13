9f39656124a3bdf8ba8c20cc111493db231f7f3a
Tsyren <Dashidymbrylov>
2025-11-13 10:51:11 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Auth\StatefulGuard;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,21 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void {}
+    public function register(): void
+    {
+        $this->app->bind(
+            StatefulGuard::class,
+            static function (): StatefulGuard {
+                $guard = Auth::guard('web');
+
+                if (!$guard instanceof StatefulGuard) {
+                    throw new \RuntimeException('Web guard must be stateful.');
+                }
+
+                return $guard;
+            }
+        );
+    }
 
     /**
      * Bootstrap any application services.
